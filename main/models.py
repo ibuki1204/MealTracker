1faa05ec61910dc1effc07b9504e206cdc4132a2
ibuki1204 <ibuki120411@i.softbank.jp>
2025-08-21 16:15:45 +0900

    cred ok

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ class Meal(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     calories = models.PositiveIntegerField()
+    
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Meal
+from .forms import MealForm
+from django.db.models import Q  # ← 検索用
 
 def index(request):
-    return render(request,'main/index.html')
-# Create your views here.
+    query = request.GET.get('q', '')  # URLパラメータ 'q' を取得
+    if query:
+        meals = Meal.objects.filter(name__icontains=query)  # 部分一致検索
+    else:
+        meals = Meal.objects.all()
+    
+    params = {
+        'title': 'Meal Tracker',
+        'message': 'Welcome to the Meal Tracker!',
+        'meals': meals,
+        'query': query,
+    }
+    return render(request, 'main/index.html', params)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = MealForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = MealForm()
+    return render(request, 'main/create.html', {'form': form})
+
+def update(request, pk):
+    meal = get_object_or_404(Meal, pk=pk)
+    if request.method == 'POST':
+        form = MealForm(request.POST, instance=meal)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = MealForm(instance=meal)
+    return render(request, 'main/update.html', {'form': form, 'meal': meal})
+
+def delete(request, pk):
+    meal = get_object_or_404(Meal, pk=pk)
+    if request.method == 'POST':
+        meal.delete()
+        return redirect('index')
+    return render(request, 'main/delete.html', {'meal': meal})
+
+
+
